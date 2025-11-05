40cec84d3343814e06f704469f295c4d059ab342
pocofrosty <dzheng24@sas.upenn.edu>
2025-11-04 20:57:16 -0500

    Instant.page

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,7 +5,7 @@
         return urlParams.get('q') || '';
     }
 
-    const query = getQuery();
+    const query = getQuery().replace(/['"']/g, ''); // Remove quotation marks
     if (!query) return;
 
     const pagefind = await import("/pagefind/pagefind.js");
@@ -16,15 +16,21 @@
     const search = await pagefind.debouncedSearch(query);
     const TotalResults = await Promise.all(search.results.map(r => r.data()));
 
-    console.log("Search results for query:", query);
-
     const resultsContainer = document.getElementById('search-results');
+    const titleElement = document.getElementById('search-results-title');
+
+    // Update the h1 element with the italicized and bolded search query
+    titleElement.innerHTML = `Search Results for <b>${query}</b>`;
 
     // Filter out results linking to the default "/" URL
     const filteredResults = TotalResults.filter(r => r.raw_url !== "/");
 
-    // Modify the excerpt to remove Title, Date, and Read Time information
+    // Convert relative URLs to absolute URLs
+    const baseUrl = window.location.origin;
     filteredResults.forEach(r => {
+        r.raw_url = `${baseUrl}${r.raw_url}`;
+
+        // Modify the excerpt to remove Title, Date, and Read Time information
         const readIndex = r.excerpt.indexOf("read");
         if (readIndex !== -1) {
             r.excerpt = r.excerpt.substring(readIndex + 4).trim();
